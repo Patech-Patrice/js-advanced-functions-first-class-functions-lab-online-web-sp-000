9b02cdfd10ebf81ad2b1b2884b33490f4d73f47d
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-08-23 20:22:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function (drivers){
-  return drivers.slice(0,2);
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2)
+  
+const returnLastTwoDrivers = drivers => drivers.slice(-2)
+   
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
+  
+const createFareMultiplier = multiplyValue => {
+    return function (aValue) {
+        return multiplyValue * aValue
+    }
 }
-const returnLastTwoDrivers = function (drivers){
-  return drivers.slice(-2);
-}
-
- const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
- function createFareMultiplier(multiplier) {
-  return function(fare){
-    return fare*multiplier
-  }
-}
-
- const fareDoubler = createFareMultiplier(2);
-const fareTripler = createFareMultiplier(3);
-
- function selectDifferentDrivers(drivers, fn){
-  return fn(drivers)
-} 
+  
+const fareDoubler = createFareMultiplier(2)
+  
+const fareTripler = createFareMultiplier(3)
+  
+const selectDifferentDrivers =  (arrayOfDrivers, randomFunc) => randomFunc(arrayOfDrivers)
+  
